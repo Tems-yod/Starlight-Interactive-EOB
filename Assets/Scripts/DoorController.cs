6d61d244f081b3db0a4101b39dc03b5e23d6f499
Tems-yod <112668080+Tems-yod@users.noreply.github.com>
2025-03-30 05:40:13 -0400

    DemoRooms and Doors

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+	[SerializeField] private Animator doorAnim = null;
+
+	private bool doorOpen = false;
+
+	[SerializeField] private string openAnimationName = "DoorAnimation";
+	[SerializeField] private string closeAnimationName = "DoorClose";
+
+	[SerializeField] private int waitTimer = 1;
+	[SerializeField] private bool pauseInteraction = false;
+
+	
+	AudioSource DoorSound;
+
+	void Start()
+    {
+        DoorSound = GetComponent<AudioSource>();
+    }
+
+	private IEnumerator PauseDoorInteraction()
+	{
+		pauseInteraction = true;
+		yield return new WaitForSeconds(waitTimer);
+		pauseInteraction = false;
+	}
+
+	public void PlayAnimation()
+	{
+		DoorSound.Play(0);
+		if(!doorOpen && !pauseInteraction)
+		{
+			doorAnim.Play(openAnimationName, 0, 0.0f);
+			doorOpen = true;
+			StartCoroutine(PauseDoorInteraction());
+		}
+		else if(doorOpen && !pauseInteraction)
+		{
+			doorAnim.Play(closeAnimationName, 0, 0.0f);
+			doorOpen = false;
+			StartCoroutine(PauseDoorInteraction());
+		}
+	}
+}
